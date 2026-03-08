725f5bda1fe1f12c52457d03f6d1b90077acdf18
Meaza Mulatu tale <149243635+kidehae@users.noreply.github.com>
2026-03-08 04:28:35 +0300

    Add solution for Continuous Subarray Sum

---
+++ leetcode/0523-Continuous-Subarray-Sum/0523-Continuous-Subarray-Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        count = { 0: -1}
+        summ = 0
+        for i in range(len(nums)):
+            summ += nums[i]
+            r = summ % k
+            if r not in count:
+                count[r] = i
+            elif i - count[r] > 1:
+                return True
+
+        return False
