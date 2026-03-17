48b94938b4ea3bc6f324f44ea9fe9c08e21c4584
Meaza Mulatu tale <149243635+kidehae@users.noreply.github.com>
2026-03-17 14:00:27 +0300

    Add solution for Patching Array

--- leetcode/0330-patching-array/0330-patching-array.py
+++ leetcode/0330-patching-array/0330-patching-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
         miss = 1
-        patches = 0
         i = 0
-        
+        p = 0
+
         while miss <= n:
-            if i < len(nums) and nums[i] <= miss:
+            if i < len(nums) and miss >= nums[i]:
                 miss += nums[i]
                 i += 1
             else:
                 miss += miss
-                patches += 1
-        return patches
-            
+                p += 1
+
+        return p
