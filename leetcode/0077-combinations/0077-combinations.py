8ebc0bac72b49072f01c30a50d99d42762db5bb0
Meaza Mulatu tale <149243635+kidehae@users.noreply.github.com>
2026-04-03 16:57:38 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -1,24 +1,20 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
 
-        res = []
 
-        def backtrack(num, path):
+        def combine(num, path):
             if len(path) == k:
                 res.append(path[:])
                 return
 
-            for i in range(num, n + 1):
+            for i in range (num, n + 1):
                 path.append(i)
-                backtrack(i + 1, path)
+                combine(i + 1, path)
                 path.pop()
 
-        backtrack(1,[])
+        res = []
+        combine(1,[])
         return res
 
-        
-
-
-        
 
         
